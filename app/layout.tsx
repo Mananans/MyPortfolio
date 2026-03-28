a698c9ea797b5ba823261dd8e7530cd9dd01e71c
Mananans <guswls0477@gmail.com>
2026-03-28 19:51:16 +0900

    260328

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/react";
+import AnalyticsWrapper from "./components/AnalyticsWrapper";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
